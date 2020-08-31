704e9de53998527e6735be449fa46fbaea998a75
Candice Han <jeeyoung.han82@gmail.com>
2020-08-31 13:25:44 +0200

    Setup for learning Redux

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count++;
+  updateText(); // notify to HTML that sth has been changed.
+};
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
