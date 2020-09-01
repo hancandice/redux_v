b961b2a9ecb0434fb449bd98f07020b0225a205e
Candice Han <jeeyoung.han82@gmail.com>
2020-09-01 12:35:08 +0200

    Import Redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  count++;
-  updateText(); // notify to HTML that sth has been changed.
-};
-const handleMinus = () => {
-  count--;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
+
+// redux is built to help to manage data.
+// reducer is a function that modifies data.
+
+// if reducer returns sth, it becomes the data of the application.
+// nobody else but only countModifier(reducer) can modify the data.
