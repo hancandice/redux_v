1ae74dd4419b07d3e72b4820521e66c1ddfa3059
Candice Han <jeeyoung.han82@gmail.com>
2020-09-04 15:06:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
 
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore);
 console.log(countStore.getState());
 
 // redux is built to help to manage data.
