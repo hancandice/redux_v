ad4f585ce5689b96eb111f49b1cb4fa47de5cb71
Candice Han <jeeyoung.han82@gmail.com>
2020-09-05 17:15:06 +0900

    RECAP redux

--- src/index.js
+++ src/index.js
@@ -4,20 +4,37 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 const countStore = createStore(countModifier);
 
-console.log(countStore);
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 // redux is built to help to manage data.
 // reducer is a function that modifies data.
